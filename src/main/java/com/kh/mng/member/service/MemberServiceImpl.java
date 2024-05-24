3cad01b5f1ff575479dcdfdeabbab300b917c7d0
LLjay <julylee0724@gmail.com>
2024-05-24 20:16:48 +0900

    수정:MemberController, MemberService, MemberServiceImpl, MemberDao, member-mapper.xml, memberEnrollCommon.jsp, memberEnrollAdmin(jsp, css), memberEnroll.js / 추가:memberAjax.js

--- src/main/java/com/kh/mng/member/service/MemberServiceImpl.java
+++ src/main/java/com/kh/mng/member/service/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.mng.location.model.vo.Location;
 import com.kh.mng.member.model.dao.MemberDao;
 import com.kh.mng.member.model.vo.Member;
 
@@ -23,7 +24,19 @@ public class MemberServiceImpl implements MemberService{
 
 	@Override
 	public int checkMemberId(String userId) {
-		int result = memberDao.checkMemberId(sqlSession, userId);
-		return result;
+		return memberDao.checkMemberId(sqlSession, userId);
+	}
+
+	@Override
+	public int insertCommonMember(Member m) {
+		return memberDao.insertCommonMember(sqlSession, m);
+	}
+
+	@Override
+	public int insertBossMember(Member m, Location loc) {
+		int result1 = memberDao.insertBossMember(sqlSession, m);
+		int result2 = memberDao.insertLocation(sqlSession, loc);
+		
+		return result1 * result2;
 	}
 }

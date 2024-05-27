c09df859cd6108420616dc0d49c55e86db158eb1
SIMGYUHONG <ebtmzjvl@naver.com>
2024-05-27 17:09:03 +0900

    추가 펫 등록기능 완료

--- src/main/java/com/kh/mng/pet/controller/PetController.java
+++ src/main/java/com/kh/mng/pet/controller/PetController.java
@@ -2,11 +2,13 @@ package com.kh.mng.pet.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.mng.member.model.vo.Member;
 import com.kh.mng.pet.model.vo.Pet;
 import com.kh.mng.pet.service.PetService;
 import com.kh.mng.pet.service.PetServiceImpl;
@@ -14,24 +16,41 @@ import com.kh.mng.pet.service.PetServiceImpl;
 @Controller
 public class PetController {
 	
-	private PetService petService = new PetServiceImpl();
+	@Autowired
+	private PetService petService;
 	
 	@RequestMapping("insertPet.mp")
 	public String insertPet(Pet p, HttpSession session, Model model) {
 		
-		int result = petService.insertPet(p);
+	    // 세션에서 로그인한 사용자 정보 가져오기
+	    Member loginUser = (Member) session.getAttribute("loginUser");
+	    // 로그인한 사용자 정보에서 사용자 번호 가져오기
+	    int userNo = loginUser.getUserNo();
+
+	    // 사용자 번호를 Member 객체에 설정
+	    p.setUserNo(userNo);
+		
+		int result = petService.insertPet(p);		
 		
 		if (result > 0) {
 			session.setAttribute("alertMsg", "반려동물 등록에 성공하셨습니다.");
-			return "redirect:";
+			return "redirect:/myPagePetInfo.mp";
 		} else {
 			model.addAttribute("errorMsg", "반려동물 등록에 실패하셨습니다.");
-			return "common/errorPage";
+			return "redirect:/myPagePetInfo.mp";
 		}
 	}
 	
-	@PostMapping("/updatePet.mp")
+	@PostMapping("updatePet.mp")
 	public String updatePet(Pet p, HttpSession session, Model model) {
+		
+	    // 세션에서 로그인한 사용자 정보 가져오기
+	    Member loginUser = (Member) session.getAttribute("loginUser");
+	    // 로그인한 사용자 정보에서 사용자 번호 가져오기
+	    int userNo = loginUser.getUserNo();
+
+	    // 사용자 번호를 Member 객체에 설정
+	    p.setUserNo(userNo);
 		
 		int result = petService.updatePet(p);
 		
@@ -42,6 +61,6 @@ public class PetController {
 		} else {
 			model.addAttribute("errorMsg", "반려동물 정보 수정 실패");
 			return "common/errorPage";
-		}		
+		}
 	}
 }

ce31bec4419b4584ce48772cffe268feb157a7a5
LLjay <julylee0724@gmail.com>
2024-06-12 09:31:49 +0900

    수정:서버 오류 안 나도록 주석 처리

--- src/main/java/com/kh/mng/member/controller/MemberSMSController.java
+++ src/main/java/com/kh/mng/member/controller/MemberSMSController.java
@@ -15,41 +15,41 @@ import net.nurigo.sdk.message.service.DefaultMessageService;
 @Controller
 public class MemberSMSController {
 	
-	final DefaultMessageService messageService;
-	
-	@Value("${sms.apiKey}") 
-	private String apiKey;
-	
-	@Value("${sms.apiSecretKey}") 
-	private String apiSecretKey;
-	
-	@Value("${sms.url}") 
-	private String apiUrl;
-	
-	@Value("${sms.sentNum}") 
-	private String sentNum;
-	
-	@GetMapping("certification.me")
-	public String test() {
-		return "";
-	}
-
-    public MemberSMSController() {
-        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, apiUrl);
-    }
-    
-    @ResponseBody
-    @PostMapping("certification.me")
-    public SingleMessageSentResponse sendCertifyCode(String getNum) {
-        Message message = new Message();
-        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-        message.setFrom(sentNum);
-        message.setTo(getNum);
-        message.setText("[멍냥가이드] 본인확인 인증번호 [" + 1 + "] ");
-
-        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-        System.out.println(response);
-
-        return response;
-    }
+//	final DefaultMessageService messageService;
+//	
+//	@Value("${sms.apiKey}") 
+//	private String apiKey;
+//	
+//	@Value("${sms.apiSecretKey}") 
+//	private String apiSecretKey;
+//	
+//	@Value("${sms.url}") 
+//	private String apiUrl;
+//	
+//	@Value("${sms.sentNum}") 
+//	private String sentNum;
+//	
+//	@GetMapping("certification.me")
+//	public String test() {
+//		return "";
+//	}
+//
+//    public MemberSMSController() {
+//        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, apiUrl);
+//    }
+//    
+//    @ResponseBody
+//    @PostMapping("certification.me")
+//    public SingleMessageSentResponse sendCertifyCode(String getNum) {
+//        Message message = new Message();
+//        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
+//        message.setFrom(sentNum);
+//        message.setTo(getNum);
+//        message.setText("[멍냥가이드] 본인확인 인증번호 [" + 1 + "] ");
+//
+//        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+//        System.out.println(response);
+//
+//        return response;
+//    }
 }

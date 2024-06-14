e662a91869bac9189b4ea252d40c0b50f4625e44
88fury <jjaman3@naver.com>
2024-06-14 17:42:33 +0900

    사장님 장소정보페이지 진행중 마이바티스 수정 로케이션 맵퍼 수정

---
+++ src/main/java/com/kh/mng/bosspage/model/vo/LocationOperationTime.java
@@ -0,0 +1,12 @@
+package com.kh.mng.bosspage.model.vo;
+
+import java.sql.Time;
+
+public class LocationOperationTime {
+	
+	private Time startTime;//시작시간
+	private Time endTime;//종료시간
+	private boolean restStatus;//휴무여부
+	private String day; //체크인 체크아웃
+	
+}
